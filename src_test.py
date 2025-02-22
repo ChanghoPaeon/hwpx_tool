4925bca7af82c882725411fc6973b09c75168b21
ChanghoPaeon <springer8729@gmail.com>
2025-02-22 09:23:49 +0900

    주석 등

--- src_test.py
+++ src_test.py
@@ -15,20 +15,24 @@ file_path = "F:\\download\\EBS 2023 올림포스 전국연합학력평가 기출
 
 file_path ="F:\git_repo\hwpx_tool\[고][2022][1-2-b][경기과천시][과천중앙고][수하][신사고][함수-조합][04017][10002][04001][그림7-0-7-0].hwp"
 
-
+# [파이썬-한/글] 보안모듈 설치방법(귀찮은 보안팝업 제거) https://employeecoding.tistory.com/67
 
 # file_path = "F:\git_repo\hwpx_tool\\test.hwp"
 doc = hwp.open(file_path)
 # content = hwp.GetTextFile()
+# 자릿수 맞춰 줘야 함?
+# 메시지 박스 타입에 맞게 설정해줘야 함
+hwp.SetMessageBoxMode(0x00000010)
 
 hwp.MoveDocBegin()
-
+# 메시지 박스 자동
 for ctrl in hwp.ctrl_list:
     # print(ctrl)
     # 미주 미주
 
     # print(ctrl.UserDesc)
     if ctrl.UserDesc == "수식":
+
         ret = hwp.select_ctrl(ctrl)
         hwp.move_to_ctrl(ctrl)
         # hwp.MoveRight()
@@ -51,38 +55,53 @@ for ctrl in hwp.ctrl_list:
 
             ctrl.Properties.SetItem("String",result)
             ctrl.Properties.SetItem("VisualString", result)
-            hwp.HAction.GetDefault("EquationModify", hwp.HParameterSet.HEqEdit.HSet)
-            # hwp.HParameterSet.SetItem("String", result)
-            print(hwp.HParameterSet.HEqEdit.string)
-            print(hwp.HParameterSet.HEqEdit)
-            hwp.HParameterSet.HEqEdit.VisualString = result
-            print(hwp.HParameterSet.HEqEdit.VisualString)
-            print(hwp.HParameterSet.HEqEdit.Version)
-            hwp.HAction.Execute("EquationModify", hwp.HParameterSet.HEqEdit.HSet)
+            # hwp.HAction.GetDefault("EquationModify", hwp.HParameterSet.HEqEdit.HSet)
+            # # hwp.HParameterSet.SetItem("String", result)
+            # print(hwp.HParameterSet.HEqEdit.string)
+            # print(hwp.HParameterSet.HEqEdit)
+            # hwp.HParameterSet.HEqEdit.VisualString = result
+            # print(hwp.HParameterSet.HEqEdit.VisualString)
+            # print(hwp.HParameterSet.HEqEdit.Version)
+            # hwp.HAction.Execute("EquationModify", hwp.HParameterSet.HEqEdit.HSet)
             # hwp.HAction.Run("SelectAll")
             # 수식 하나 바꾼 후 죽음 이게 원인인가
-            hwp.HAction.Run("Delete")
-
+            # hwp.HAction.Run("Delete")
+            # 바뀌긴 하나 파일 수식 열어서 편집창 등왔다리 갔다리 해야 변경 적용됨 이유는 뭘까?
             # pset = hwp.HParameterSet.HEqEdit
-            # hwp.HAction.GetDefault("EquationModify", pset.HSet)
-            # pset.string = pset.VisualString
-            # pset.Version = "Equation Version 60"
-            # hwp.HAction.Execute("EquationModify", pset.HSet)
+            # hwp.HAction.GetDefault("EquationModify", hwp.HParameterSet.HEqEdit.HSet)
+            # hwp.HParameterSet.HEqEdit.string = result
+            # hwp.HParameterSet.HEqEdit.VisualString = result
+            # # hwp.HParameterSet.HEqEdit.Version = "Equation Version 60"
+            # hwp.HAction.Execute("EquationModify", hwp.HParameterSet.HEqEdit.HSet)
+            # hwp.Run("Cancel")  # 폰트 예뻐짐
+            # hwp.Run("MoveRight")  # 다음 수식 삽입 준비
+            # hwp.MoveRight()
 
+            # https://employeecoding.tistory.com/335
+            # act = hwp.CreateAction("EquationPropertyDialog")
+            # pset = act.CreateSet()
+            # act.GetDefault(pset)
+            #
+            # pset.SetItem("String", result)
+            # act.Execute(pset)
 
             hwp.HAction.GetDefault("EquationCreate", hwp.HParameterSet.HEqEdit.HSet)
             hwp.HParameterSet.HEqEdit.EqFontName = "HancomEQN"
+            hwp.HParameterSet.HEqEdit.EqFontName = "HYhwpEQ"
             hwp.HParameterSet.HEqEdit.string = result
+            hwp.HParameterSet.HEqEdit.TreatAsChar = True
             hwp.HParameterSet.HEqEdit.BaseUnit = hwp.PointToHwpUnit(11.0)
+            hwp.HParameterSet.HEqEdit.Version  = "Equation Version 60"
             hwp.HAction.Execute("EquationCreate", hwp.HParameterSet.HEqEdit.HSet)
 
+            hwp.HAction.Run("Delete")
 
             # hwp.HAction.GetDefault("EquationPropertyDialog", hwp.HParameterSet.HShapeObject.HSet)
             # hwp.HParameterSet.HShapeObject.string = result
             # hwp.HAction.Execute("EquationPropertyDialog", hwp.HParameterSet.HShapeObject.HSet)
 
             #
-            # hwp.MoveRight()
+            hwp.MoveRight()
             # hwp.BreakPara()
             # hwp.set_font(TextColor="Red")
             # hwp.insert_text(ctrl.Properties.Item("String"))
