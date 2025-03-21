5a2643df496551748d15292410f036b3e213d0bd
springer <springer8729@gmail.com>
2025-03-22 00:08:14 +0900

    쿼리문과 파일명 연동

--- workbook_publisher.py
+++ workbook_publisher.py
@@ -31,11 +31,11 @@ import pandas as pd
 # hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule")  # 보안모듈
 
 class workbook_publisher:
-    def __init__(self, folder_path="./", output_path = "./db"):
+    def __init__(self, folder_path="./", output_path = "./db", db_path="./"):
         """
         """
         self.folder_path = folder_path
-        self.db_path = os.path.join(folder_path, "수1-수능특강-ex+lv1.hwp")
+        self.db_path = db_path
         self.working_path = folder_path
         self.hwp = pyhwpx.Hwp()
         self.hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule")
@@ -91,7 +91,6 @@ class workbook_publisher:
 
         return hwp
 
-
     def set_multi_col(self):
 
         #  단 설정
@@ -111,10 +110,16 @@ class workbook_publisher:
         # hwp.XHwpWindows.Active_XHwpWindow.Visible = False
 
     def publish_workbook(self):
+        print("Reading DB Start")
         df = pd.read_csv("./db/db.csv", encoding='euc-kr')
         q_result = df.query('문제타입 == "ex" or 문제타입 == "lv1"')
 
-        q_result = df.query('문제타입 == "lv1"')
+        prob_lv = "ex"
+        prob_subj = "확통"
+        qeury = "문제타입 == \"" + prob_lv + "\" and " + "과목 ==" +"\""+ prob_subj+"\""
+        print(qeury)
+        q_result = df.query(qeury)
+        prob_type = "문제타입 == " + prob_lv
 
         # type_order = {"ex": 0, "lv1": 1, "lv2": 2, "lv3": 3}
         # df_sorted = q_result.sort_values(by=["문제집", "문제타입"], ascending=[False, True], key=lambda x: x.map(type_order))
@@ -130,12 +135,13 @@ class workbook_publisher:
 
         for _, row in df_sorted.iterrows():
             # print(f"문제집: {row['문제집']}, 문제타입: {row['문제타입']}")
-            print(self.folder_path)
-            print(str(row['문제집']) + "-" +  str(row['문제번호']).zfill(4)+"-A")
-            print(os.path.join(self.folder_path), str(row['문제집']) + "-" +  str(row['문제번호']).zfill(4)+"-A")
+            # print(self.db_path)
+            current_prob = str(row['문제집']) + "-" +  str(row['문제번호']).zfill(4)+"-A"
+            print("!!! >>  " + current_prob )
+            print(os.path.join(self.db_path), current_prob)
 
 
-            folder = Path(self.folder_path)
+            folder = Path(self.db_path)
             file_path = folder / f"{row['문제집']}-{str(row['문제번호']).zfill(4)}-A"
 
             hwp.Open(str(file_path))
@@ -192,8 +198,9 @@ class workbook_publisher:
         hwp_tool.adjust_image_width(hwp)
         hwp_tool.adjust_table_width(hwp)
 
-        hwp.save_as("E:\\workspace\\ebs\\수특-수1-lv1.hwp", "HWP")
-        hwp.save_as("E:\\workspace\\ebs\\수특-수1-lv1.hwpx", "HWPX")
+        save_name = "E:\\workspace\\ebs\\" + "수특-"+prob_subj+"-"+prob_lv
+        hwp.save_as(save_name + ".hwp", "HWP")
+        hwp.save_as(save_name +".hwpx", "HWPX")
 
         return
 
@@ -204,48 +211,7 @@ class workbook_publisher:
 
 
 
-    def create_hwp_doc(self):
-        hwp.add_doc()
-        # hwp.Open(file_path + ".hwp")
-
-        # 페이지 여백 설정
-        act = hwp.CreateAction("PageSetup")
-        pset = act.CreateSet()
-        act.GetDefault(pset)
-        pset.SetItem("ApplyTo", 3)
-
-        item_set = pset.CreateItemSet("PageDef", "PageDef")
-        margin = hwp.MiliToHwpUnit(10)
-        item_set.SetItem("TopMargin", margin)
-        item_set.SetItem("BottomMargin", margin)
-        item_set.SetItem("LeftMargin", margin)
-        item_set.SetItem("RightMargin", margin)
-        item_set.SetItem("HeaderLen", margin)
-        item_set.SetItem("FooterLen", margin)
-        item_set.SetItem("GutterLen", margin)
-
-        act.Execute(pset)
-
-        return hwp
-
-    def set_multi_col(self):
-
-        #  단 설정
-        # 파라미터셋._prop_map_get_.keys()
-        # https://www.inflearn.com/community/questions/1077679/%ED%95%9C%EA%B8%80-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EB%B0%94%ED%83%95%EC%AA%BD-%EB%8B%A4%EB%8B%A8?srsltid=AfmBOorgKdk__dZPqReicG_VmRNLMrrEm6LP6nHglMw_9Ud5QRInHfSL
-        pset = hwp.HParameterSet.HColDef
-        hwp.HAction.GetDefault("MultiColumn", pset.HSet)
-        pset.Count = 2
-        pset.SameSize = 1
-        pset.LineType = 1
-        pset.SameGap = hwp.MiliToHwpUnit(8.0)  # <--
-        pset.HSet.SetItem("ApplyClass", 832)
-        pset.HSet.SetItem("ApplyTo", 6)
-        hwp.HAction.Execute("MultiColumn", pset.HSet)
-        # act.Execute(pset)
-
-        # hwp.XHwpWindows.Active_XHwpWindow.Visible = False
 
 
-maker = workbook_publisher("C:\\Data\\DB\\DB_src_2", "C:\\Data\\")
+maker = workbook_publisher("C:\\Data\\", "C:\\Data\\", "C:\\Data\\DB")
 maker.publish_workbook()
